da95bec9b315cbc79ac91e966d41eb4a75b56ac6
caledep <caledep@live.com>
2024-05-25 19:49:46 -0600

    added styling for education page

--- src/views/Education.vue
+++ src/views/Education.vue
@@ -4,72 +4,80 @@
       <div class="top-block">
          <div class="resume-block">
             <div class="skills-block">
-               <div>
+               <div class="cell">
                   <h1>{{ title.hSkillTitle }}</h1>
                   <hr>
-                  <li v-for="hSkill in hSkills">
-                     {{ hSkill.skill }}
-                  </li>
+                  <div>
+                     <li v-for="hSkill in hSkills">
+                        {{ hSkill.skill }}
+                     </li>
+                  </div>
                </div>
-               <div>
+               <div class="cell">
                   <h1>{{ title.sSkillTitle }}</h1>
                   <hr>
-                  <li v-for="sSkill in sSkills">
-                     {{ sSkill.skill }}
-                  </li>
+                  <div>
+                     <li v-for="sSkill in sSkills">
+                        {{ sSkill.skill }}
+                     </li>
+                  </div>
                </div>
             </div>
             <div class="edu-block">
-               <div class="education">
+               <div class="education cell">
                   <h1>{{ title.eduTitle }}</h1>
                   <hr>
                   <div class="degrees">
                      <div>
-                        <h3>{{ title.eduSubTitle1 }}</h3>
+                        <h2>{{ title.eduSubTitle1 }}</h2>
                         <p>{{ desc.edu1 }}</p>
                         <p>{{ desc.eduLoc1 }}</p>
                         <p>{{ desc.eduDate1 }}</p>
                      </div>
                      <div>
-                        <h3>{{ title.eduSubTitle2 }}</h3>
+                        <h2>{{ title.eduSubTitle2 }}</h2>
                         <p>{{ desc.edu2 }}</p>
                         <p>{{ desc.eduLoc2 }}</p>
                         <p>{{ desc.eduDate2 }}</p>
                      </div>
                   </div>
                </div>
-               <div class="achievements">
+               <div class="achievements cell">
                   <h1>{{ title.achiTitle }}</h1>
                   <hr>
                   <div>
-                     <h3>{{ title.achiSubTitle1 }}</h3>
+                     <h2>{{ title.achiSubTitle1 }}</h2>
                      <h4>{{ desc.achi1 }}</h4>
                      <p>{{ desc.achiDesc1 }}</p>
                   </div>
                   <div>
-                     <h3>{{ title.achiSubTitle2 }}</h3>
+                     <h2>{{ title.achiSubTitle2 }}</h2>
                      <h4>{{ desc.achi2 }}</h4>
                      <p>{{ desc.achiDesc2 }}</p>
                   </div>
                   <div>
-                     <h3>{{ title.achiSubTitle3 }}</h3>
+                     <h2>{{ title.achiSubTitle3 }}</h2>
                      <h4>{{ desc.achi3 }}</h4>
                      <p>{{ desc.achiDesc3 }}</p>
                   </div>
                </div>
             </div>
          </div>
-         <div class="tech-block">
+         <div class="tech-block cell">
             <h1>{{ title.tSkillTitle }}</h1>
             <hr>
-            <h3>{{ title.tSkillSubTitle1 }}</h3>
+            <div>
+            <h2>{{ title.tSkillSubTitle1 }}</h2>
             <li v-for="tSkill in tSkills1">
                {{ tSkill.skill }}
             </li>
-            <h3>{{ title.tSkillSubTitle2 }}</h3>
+         </div>
+         <div>
+            <h2>{{ title.tSkillSubTitle2 }}</h2>
             <li v-for="tSkill in tSkills2">
                {{ tSkill.skill }}
             </li>
+         </div>
          </div>
       </div>
       <div class="bottom-block">
@@ -84,7 +92,7 @@
                   sSkillTitle: "Soft Skills",
                   tSkillTitle: "Technical Skills",
                   tSkillSubTitle1: "Language Proficiencies:",
-                  tSkillSubTitle2: "Skills:",
+                  tSkillSubTitle2: "Additional Skills:",
                   eduTitle: "Education",
                   eduSubTitle1: "Associates of Science in Computer Science",
                   eduSubTitle2: "Bachelors of Science in Orginizational Management",
@@ -130,9 +138,7 @@
                          { skill: 'c#'},
                          { skill: 'JavaScript'},
                          { skill: 'HTML/CSS'},
-                         { skill: 'sass'},
                          { skill: 'python'},
-                         { skill: 'R'},
                          { skill: 'SQL'},
                          { skill: 'Bash'},
                          { skill: 'Vue'}])
@@ -153,13 +159,94 @@
                          { skill: 'Unit Testing/e2e Testing'},
                          { skill: 'Code Reviews & Feedback'},
                          { skill: 'Technical Documentation'},
-                         { skill: 'Talking about technical ideas with technical and non-technical audiences'},
+                         { skill: 'Speaking about technical ideas with technical and non-technical audiences'},
                          { skill: 'Project Tracking Software'}])
-
-
 </script>
 <style lang="scss" scoped>
-* {
-    outline: 1px solid red !important;
+main {
+   display: flex;
+   flex-direction: column;
+   gap: 2rem;
+   color: var(--primary);
+
+   li {
+      font-size: 22px;
+
+      position: -12px;
+      padding-left: 20px;
+   }
+   p {
+      font-size: 18px;
+   }
+   hr {
+      border-color: var(--primary-hover);
+   }
+   .cell {
+      padding: 2rem;
+      background: #FFFCE2;
+      border: solid;
+      border-width: 0.2rem;
+      border-color: var(--primary);
+      border-radius:2em; 
+   }
+   .top-block {
+      display: flex;
+      flex-flow: row;
+      flex-wrap: wrap;
+      gap: 2rem;
+   }
+   .bottom-block {
+      max-width: 87rem;
+   }
+   .resume-block {
+      display: flex;
+      flex-direction: column;
+      gap: 2rem;
+   }
+   .skills-block {
+      display: flex;
+      flex-flow: row;
+      flex-wrap:wrap;
+      max-width: 60rem;
+      gap: 2rem;
+      div {
+         flex-grow: 1;
+         li:first-child {
+            padding-block-start: 1rem;
+         }
+      }
+   }
+   .edu-block {
+      display: flex;
+      flex-direction: column;
+      max-width: 60rem;
+      gap: 2rem;
+   }
+   .tech-block {
+      display: flex;
+      flex-direction: column;
+      flex-grow: 1;
+      flex-shrink: 1;
+      max-width: 25rem;
+      min-width: 20rem;
+      h2 {
+         padding-top: 2rem;
+         padding-bottom: 1rem;
+      }
+   }
+   .degrees {
+      display: flex;
+      gap: 2rem;
+      padding-top: 1rem;
+      div{
+         text-align: center;
+         max-width: 24rem;
+      }
+   }
+   .achievements {
+      div {
+         padding-top: 1rem;
+      }
+   }
 }
 </style>
