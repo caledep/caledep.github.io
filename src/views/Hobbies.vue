aafc18e40240cab87d109b0ca249e362b5e45554
caledep <caledep@live.com>
2024-06-11 14:45:32 -0600

    update construction pages

--- src/views/Hobbies.vue
+++ src/views/Hobbies.vue
@@ -1,36 +1,86 @@
 <template>
-  <main class="hobbies-page">
-    <div class="card">
-      <h1>Hobbies</h1>
+  <main class="projects-page">
+    <div class="header">
+      <div class="card">
+        <h1>Hobbies</h1>
+      </div>
     </div>
-    <p class="construction_notice">
-      <img
-        src="../assets/images/icon_construction_dark.svg"
-        class="icon"
-        alt="construction icon"
-      />
-      <b>This page is currently undergoing development, check back later.</b>
-      <img
-        src="../assets/images/icon_construction_dark.svg"
-        class="icon"
-        alt="construction icon"
-      />
-    </p>
+    <div class="body">
+      <p class="construction_notice">
+        <img
+          src="../assets/images/icon_construction_dark.svg"
+          class="icon"
+          alt="construction icon"
+        />
+        <b class="notice"
+          >This page is currently undergoing development, check back later.</b
+        >
+        <img
+          src="../assets/images/icon_construction_dark.svg"
+          class="icon"
+          alt="construction icon"
+        />
+      </p>
+    </div>
   </main>
 </template>
 <script setup></script>
 <style scoped>
+.projects-page {
+  max-width: 87rem;
+  color: var(--primary);
+}
 .construction_notice {
   display: flex;
-  width: 100%;
-  justify-content: center;
+  width: fit-content;
+  justify-content: space-between;
   align-items: center;
+  text-align: center;
   font-size: 1.5rem;
+  padding: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border: solid;
+  background: #fffce2;
+  border-width: 3px;
+  border-color: #161a4a;
+  border-radius: 1.5em;
 }
-.card {
+.notice {
+  padding-left: 4rem;
+  padding-right: 4rem;
+  @media (max-width: 768px) {
+    padding: 0rem;
+  }
+}
+.header {
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 1.5rem;
+  padding: 1rem;
+}
+.body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: 1.5rem;
+  padding: 1rem;
+}
+.card {
+  padding: 2rem;
+  padding-top: 1rem;
+  padding-bottom: 1rem;
+  border: solid;
+  background: #fffce2;
+  border-width: 3px;
+  border-color: #161a4a;
+  border-radius: 1.5em;
+
+  @media (max-width: 768px) {
+    padding: 1rem;
+    font-size: 16px;
+    border-radius: 2em;
+  }
 }
 </style>
